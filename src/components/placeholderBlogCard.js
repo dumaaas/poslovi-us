b4d70d8e1db2d3be3c5ba68424206c38a67e8f71
dumaaas <markodumnic8@gmail.com>
2023-04-17 01:10:31 +0200

    small fix

--- src/components/placeholderBlogCard.js
+++ src/components/placeholderBlogCard.js
@@ -2,7 +2,7 @@ export default function placeholderBlogCard() {
   return (
     <div className="w-full flex flex-col rounded-xl overflow-hidden p-4 transition-all duration-200 ease-in-out relative h-[181px] bg-white shadow-[0_0_10px_rgba(0,0,0,0.1)]">
       <div className="flex gap-[15px] items-center">
-        <div className="shine-anim w-[100px] h-[100px] rounded-[8px] bg-gray-300"></div>
+        <div className="shine-anim min-w-[100px] max-w-[100px] h-[100px] rounded-[8px] bg-gray-300"></div>
         <div className="flex flex-col gap-[10px] w-full h-full">
           <div className="shine-anim w-[40%] h-[24px] rounded-[8px] bg-gray-300"></div>
           <div className="shine-anim w-full h-full rounded-[8px] bg-gray-300"></div>
